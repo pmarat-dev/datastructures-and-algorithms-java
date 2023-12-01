86a585c288577dfd0e23c4f791f8261f9d5d5e09
Piotr <pmarat.dev@gmail.com>
2023-12-01 01:16:36 +0100

    Added IsAnagram.

---
+++ src/main/java/neetcode/arrayshashing/IsAnagram.java
@@ -0,0 +1,31 @@
+package neetcode.arrayshashing;
+
+import java.util.Arrays;
+
+import static java.lang.System.out;
+
+public class IsAnagram {
+    public static boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
+
+        int[] store = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            store[s.charAt(i) - 'a']++;
+            store[t.charAt(i) - 'a']--;
+        }
+
+        for (int n : store)
+            if (n != 0) return false;
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String[] words = new String[]{"rat", "car"};
+        out.format("Is anagram (%s): %s.%n", Arrays.toString(words), isAnagram(words[0], words[1]));
+
+        words = new String[]{"anagram", "nagaram"};
+        out.format("Is anagram (%s): %s.%n", Arrays.toString(words), isAnagram(words[0], words[1]));
+    }
+}
