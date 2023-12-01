641a36f7e0a4e5deee993076f4533a4be8448efb
Piotr <pmarat.dev@gmail.com>
2023-12-01 01:31:11 +0100

    Added TwoSum.

---
+++ src/main/java/neetcode/arrayshashing/TwoSum.java
@@ -0,0 +1,33 @@
+package neetcode.arrayshashing;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+import static java.lang.System.out;
+
+public class TwoSum {
+    public static int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> prevMap = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+            int diff = target - num;
+
+            if (prevMap.containsKey(diff)) {
+                return new int[]{prevMap.get(diff), i};
+            }
+
+            prevMap.put(num, i);
+        }
+
+        return new int[]{};
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{1, 3, 11, 4, 5, 9};
+        int target = 8;
+
+        out.format("Sum of target (%s, %s): %s%n",
+                Arrays.toString(nums), target, Arrays.toString(twoSum(nums, target)));
+    }
+}
